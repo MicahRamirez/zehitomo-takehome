4ee6a8f1287733e598f3f76fed3e83391500078a
MicahRamirez <xaviermicahramirez@utexas.edu>
2020-10-07 21:23:32 +0900

    ok trying to directly write stream to client, works locally

--- pages/api/downloadImage.ts
+++ pages/api/downloadImage.ts
@@ -17,36 +17,41 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     responseType: "stream",
   })
     .then((response) => {
-      return new Promise((resolve, reject) => {
-        response.data
-          .pipe(fs.createWriteStream(imagePath))
-          .on("finish", () => {
-            return resolve();
-          })
-          .on("error", () => {
-            return reject();
-          });
+      // const readStream = fs.createReadStream(filePath);
+      res.writeHead(200, {
+        "Content-Type": "image/jpeg",
       });
+      response.data.pipe(res);
+      // return new Promise((resolve, reject) => {
+      //   response.data
+      //     .pipe(fs.createWriteStream(imagePath))
+      //     .on("finish", () => {
+      //       return resolve();
+      //     })
+      //     .on("error", () => {
+      //       return reject();
+      //     });
+      // });
     })
     .catch((error) =>
       console.error("Unable to write image to temporary directory", error)
     );
   // read and write file
-  const filePath = path.resolve(process.cwd(), imagePath);
-  const stat = fs.statSync(filePath);
-  res.writeHead(200, {
-    "Content-Type": "image/jpeg",
-    "Content-Length": stat.size,
-  });
-  // read the image and send
-  const readStream = fs.createReadStream(filePath);
-  readStream.pipe(res);
+  // const filePath = path.resolve(process.cwd(), imagePath);
+  // const stat = fs.statSync(filePath);
+  // res.writeHead(200, {
+  //   "Content-Type": "image/jpeg",
+  //   "Content-Length": stat.size,
+  // });
+  // // read the image and send
+  // const readStream = fs.createReadStream(filePath);
+  // readStream.pipe(res);
   // clean up
-  fs.unlink(filePath, (err) => {
-    if (err) {
-      console.error("Unable to delete temporary image");
-    }
-  });
+  // fs.unlink(filePath, (err) => {
+  //   if (err) {
+  //     console.error("Unable to delete temporary image");
+  //   }
+  // });
 };
 
 export default handler;
